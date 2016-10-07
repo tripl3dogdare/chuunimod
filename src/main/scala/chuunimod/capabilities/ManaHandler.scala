0a2598c25827886da093b73d7fccaaa01600dc55
tripl3dogdare <connor@connorjulian.org>
2016-10-06 21:24:31 -0600

    Fix ManaHandler issues/add updateClient(player)
    Adds a bunch of utilities and fixes to ManaHandler, including the
    ability to actually update the client.

--- src/main/scala/chuunimod/capabilities/ManaHandler.scala
+++ src/main/scala/chuunimod/capabilities/ManaHandler.scala
@@ -1,42 +1,61 @@
 package chuunimod.capabilities
 
-import net.minecraftforge.common.capabilities.ICapabilitySerializable
-import net.minecraft.nbt.NBTTagCompound
-import net.minecraftforge.common.capabilities.CapabilityInject
-import net.minecraftforge.common.capabilities.Capability
-import net.minecraft.util.EnumFacing
-import net.minecraftforge.common.capabilities.Capability.IStorage
-import net.minecraft.nbt.NBTBase
-import net.minecraftforge.fml.common.network.simpleimpl.IMessage
+import java.util.concurrent.Callable
+
+import chuunimod.ChuuniMod
+import chuunimod.capabilities.MessageUpdateClientMana.Handler
 import io.netty.buffer.ByteBuf
+import net.minecraft.client.Minecraft
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.entity.player.EntityPlayerMP
+import net.minecraft.nbt.NBTBase
+import net.minecraft.nbt.NBTTagCompound
+import net.minecraft.util.EnumFacing
+import net.minecraftforge.common.capabilities.Capability
+import net.minecraftforge.common.capabilities.Capability.IStorage
+import net.minecraftforge.common.capabilities.CapabilityInject
+import net.minecraftforge.common.capabilities.ICapabilitySerializable
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext
-import net.minecraft.entity.player.EntityPlayer
-import net.minecraft.client.Minecraft
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper
+import net.minecraftforge.fml.relauncher.Side
 
-trait ManaHandler {
-	var mana,maxMana,manaRegen:Float = 0
+trait ManaHandlerLike {
+	var mana,maxMana,manaRegen:Float
 	var dirty:Boolean = true
 	
-	def set(mana:Float=this.mana, maxMana:Float=this.mana, manaRegen:Float=this.manaRegen) {
-		this.mana = mana
-		this.maxMana = maxMana
-		this.manaRegen = this.manaRegen
-		dirty = true
+	def setMana(amt:Float) = { mana = amt; dirty = true }
+	def setMaxMana(amt:Float) = { maxMana = amt; dirty = true }
+	def setManaRegen(amt:Float) = { manaRegen = amt; dirty = true }
+	
+	def consumeMana(amt:Float):Boolean = if(mana < amt) false else { setMana(mana-amt); true; }
+	def regenMana(amt:Float):Boolean = if(mana == maxMana) false else { setMana(Math.min(mana+amt, maxMana)); true; }
+	
+	def copyTo(other:ManaHandlerLike, copyCurrent:Boolean=true) { 
+		if(copyCurrent) other.setMana(mana)
+		other.setMaxMana(maxMana)
+		other.setManaRegen(manaRegen)
 	}
 	
-	def consumeMana(amt:Float):Boolean = if(mana < amt) false else { set(mana=mana-amt); true; }
-	def regenMana(amt:Float):Boolean = if(mana == maxMana) false else { set(mana=Math.min(mana+manaRegen, maxMana)); true; }
-	
+	def updateClient(player:EntityPlayer) = if(!player.worldObj.isRemote) { 
+		if(dirty) ChuuniMod.network.sendTo(new MessageUpdateClientMana(this), player.asInstanceOf[EntityPlayerMP])
+		dirty = false
+	}
 }
+
+abstract class ManaHandler(var mana:Float = 0, var maxMana:Float = 0, var manaRegen:Float = 0) extends ManaHandlerLike
 
 object ManaHandler {
 	@CapabilityInject(classOf[ManaHandler]) final val CAP:Capability[ManaHandler] = null
 	
-	class DefaultHandler(cur:Float=0,max:Float=250,regen:Float=.25f) extends ManaHandler with ICapabilitySerializable[NBTTagCompound] {
-		set(mana=cur, maxMana=max, manaRegen=regen)
-		
-		def hasCapability(capability:Capability[_], f:EnumFacing) = { capability == ManaHandler.CAP; }
+	def instanceFor(player:EntityPlayer) = player.getCapability(CAP, null)
+	def getStorageInstance = new DefaultStorage
+	def getHandlerFactory = new Callable[DefaultHandler] { def call = new DefaultHandler }
+	
+	class DefaultHandler(cur:Float=0,max:Float=250,regen:Float=.25f) extends ManaHandler(cur,max,regen) with ICapabilitySerializable[NBTTagCompound] {
+		def hasCapability(capability:Capability[_], f:EnumFacing) = capability == ManaHandler.CAP
 		def getCapability[T](capability:Capability[T], f:EnumFacing) = { if(capability == ManaHandler.CAP) this else null }.asInstanceOf[T]
 		
 		def serializeNBT:NBTTagCompound = {
@@ -47,15 +66,12 @@ object ManaHandler {
 			nbt
 		}
 		
-		def deserializeNBT(nbt:NBTTagCompound) = set(
-			mana = nbt.getFloat("mana"),
-			maxMana = nbt.getFloat("maxMana"),
+		def deserializeNBT(nbt:NBTTagCompound) {
+			mana = nbt.getFloat("mana")
+			maxMana = nbt.getFloat("maxMana")
 			manaRegen = nbt.getFloat("manaRegen")
-		)
-	}
-	
-	object DefaultHandler {
-		def get = new DefaultHandler
+			dirty = true
+		}
 	}
 	
 	class DefaultStorage extends IStorage[ManaHandler] {
@@ -64,23 +80,28 @@ object ManaHandler {
 	}
 }
 	
-class MessageUpdateClientMana extends IMessage {
+class MessageUpdateClientMana(mh:ManaHandlerLike) extends IMessage with ManaHandlerLike {
 	var mana,maxMana,manaRegen:Float = 0
+	if(mh != null) mh.copyTo(this)
 	
-	def this(mh:ManaHandler) { this; this.mana = mh.mana; this.maxMana = mh.maxMana; this.manaRegen = mh.manaRegen }
+	def this() = this(null)
 	
 	def fromBytes(buf:ByteBuf) { this.mana = buf.readFloat; this.maxMana = buf.readFloat; this.manaRegen = buf.readFloat }
 	def toBytes(buf:ByteBuf) { List(this.mana, this.maxMana, this.manaRegen) foreach buf.writeFloat }
-	
+}
+
+object MessageUpdateClientMana {
 	class Handler extends IMessageHandler[MessageUpdateClientMana, IMessage] {
 		def onMessage(msg:MessageUpdateClientMana, ctx:MessageContext):IMessage = {
-			Minecraft.getMinecraft.addScheduledTask(new Runnable { def run = () => {
+			Minecraft.getMinecraft.addScheduledTask(new Runnable { def run = {
 				val player = Minecraft.getMinecraft().thePlayer;
-				val mh = player.getCapability(ManaHandler.CAP, null)
+				val mh = ManaHandler.instanceFor(player)
 				
-				mh.set(mana=msg.mana, maxMana=msg.maxMana, manaRegen=msg.manaRegen)
+				msg.copyTo(mh)
 			}})
 			null
 		}
 	}
+	
+	def register(net:SimpleNetworkWrapper, id:Int) = net.registerMessage(classOf[MessageUpdateClientMana.Handler], classOf[MessageUpdateClientMana], id, Side.CLIENT)
 }
