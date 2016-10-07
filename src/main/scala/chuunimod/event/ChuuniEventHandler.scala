21b29cef6e522d9a1d972a3e9a134753c4742d72
tripl3dogdare <connor@connorjulian.org>
2016-10-06 21:25:35 -0600

    Add ChuuniEventHandler
    Currently contains the code to handle adding/updating mana. Will
    eventually contain or dispatch all event code.

---
+++ src/main/scala/chuunimod/event/ChuuniEventHandler.scala
@@ -0,0 +1,47 @@
+package chuunimod.event
+
+import chuunimod.ChuuniMod
+import chuunimod.capabilities.ManaHandler
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.util.ResourceLocation
+import net.minecraftforge.event.AttachCapabilitiesEvent
+import net.minecraftforge.event.entity.player.PlayerEvent
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.gameevent.TickEvent.Phase
+import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent
+
+class ChuuniEventHandler {
+	
+	//===== CAPABILITIES =====//
+	
+	@SubscribeEvent def addPlayerCapabilities(e:AttachCapabilitiesEvent.Entity) {
+		if(e.getEntity.isInstanceOf[EntityPlayer])
+			e.addCapability(new ResourceLocation(ChuuniMod.MODID, "ManaHandler"), new ManaHandler.DefaultHandler())
+	}
+	
+	@SubscribeEvent def persistPlayerCapabilities(e:PlayerEvent.Clone) {
+		if(e.isWasDeath()) {
+			val cnew = ManaHandler.instanceFor(e.getEntityPlayer())
+			val cold = ManaHandler.instanceFor(e.getOriginal())
+			
+			cold.copyTo(cnew, false)
+		}
+	}
+	
+	//===== PLAYER TICK =====//
+	
+	@SubscribeEvent def onPlayerTick(e:PlayerTickEvent) {
+		if(e.phase != Phase.END) return
+		
+		updatePlayerMana(e.player)
+	}
+	
+	def updatePlayerMana(player:EntityPlayer) = if(!player.worldObj.isRemote) {
+		val mh = ManaHandler.instanceFor(player)
+		
+		mh.regenMana(mh.manaRegen)
+		mh.updateClient(player)
+	}
+	
+}
