057ac9372f17f99292a69b7f348adadbc021b88a
tripl3dogdare <connor@connorjulian.org>
2016-10-15 14:24:48 -0600

    Create Rikka set, update tab icon

--- src/main/scala/chuunimod/Registry.scala
+++ src/main/scala/chuunimod/Registry.scala
@@ -1,7 +1,14 @@
 package chuunimod
 
+import chuunimod.item.ItemArmorPairingManaWeapon
+import chuunimod.item.ItemCharacterArmor
 import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.init.Items
+import net.minecraft.inventory.EntityEquipmentSlot
+import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.registry.GameRegistry
 
 object Registry {
 	def preInit {
@@ -13,13 +20,25 @@ object Registry {
 }
 
 object MiscRegistry {
-	var tabMain:CreativeTabs = null
+	val tabMain:CreativeTabs = new CreativeTabs("chuunimod.main") { def getTabIconItem = ItemRegistry.itemRikkaArmor }
 	
-	def preInit {
-		tabMain = new CreativeTabs("chuunimod.main") { def getTabIconItem = Items.ENDER_EYE }
+	def preInit {}
+}
+
+object ItemRegistry {
+	val itemRikkaArmor = register(new ItemCharacterArmor("tweseal", EntityEquipmentSlot.HEAD), "tweseal")
+	
+	val itemRikkaWeapon = register(new ItemArmorPairingManaWeapon(10.0f, 1d, 1, 200, itemRikkaArmor) {
+		override def getArmoredAttackDmg(stack:ItemStack) = baseAttackDmg*2
+	}, "sspm2")
+	
+	def preInit {}
+	
+	def register[T <: Item](item:T, name:String=null):T = {
+		if(name != null) item.setUnlocalizedName(ChuuniMod.MODID+"."+name).setRegistryName(name)
+		GameRegistry.register(item)
 	}
 }
 
-object ItemRegistry { def preInit {} }
 object BlockRegistry { def preInit {} }
 object CraftingRegistry { def preInit {} }
