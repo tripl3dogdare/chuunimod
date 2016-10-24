ba0c716824f14d4460dea31e7c5c1c694c5ef84f
tripl3dogdare <connor@connorjulian.org>
2016-10-24 17:01:56 -0600

    Update proxy and event handler to new system

--- src/main/scala/chuunimod/event/ChuuniEventHandler.scala
+++ src/main/scala/chuunimod/event/ChuuniEventHandler.scala
@@ -11,12 +11,11 @@ import net.minecraft.entity.monster.EntityGhast
 import net.minecraft.entity.monster.EntityMob
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.util.ResourceLocation
-import net.minecraftforge.common.MinecraftForge
+import net.minecraft.util.text.TextComponentString
 import net.minecraftforge.event.AttachCapabilitiesEvent
 import net.minecraftforge.event.entity.EntityJoinWorldEvent
 import net.minecraftforge.event.entity.living.LivingDeathEvent
 import net.minecraftforge.event.entity.player.PlayerEvent
-import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent.Phase
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent
@@ -27,46 +26,21 @@ class ChuuniEventHandler {
 	
 	@SubscribeEvent def addPlayerCapabilities(e:AttachCapabilitiesEvent.Entity) {
 		if(e.getEntity.isInstanceOf[EntityPlayer]) {
-			e.addCapability(new ResourceLocation(ChuuniMod.MODID, "ManaHandler"), ManaHandler.getHandlerInstance)
-			e.addCapability(new ResourceLocation(ChuuniMod.MODID, "LevelHandler"), LevelHandler.getHandlerInstance)
+			ManaHandler.attachToPlayer(e)
+			LevelHandler.attachToPlayer(e)
 		}
 	}
 	
 	@SubscribeEvent def persistPlayerCapabilities(e:PlayerEvent.Clone) {
-		ManaHandler.instanceFor(e.getOriginal).copyTo(ManaHandler.instanceFor(e.getEntityPlayer), !e.isWasDeath)
-		LevelHandler.instanceFor(e.getOriginal).copyTo(LevelHandler.instanceFor(e.getEntityPlayer))
+		ManaHandler.persistOnPlayer(e)
+		LevelHandler.persistOnPlayer(e)
 	}
 	
 	@SubscribeEvent def updateClientOnJoinWorld(e:EntityJoinWorldEvent) {
 		val player = if(e.getEntity.isInstanceOf[EntityPlayer]) e.getEntity.asInstanceOf[EntityPlayer] else return
 		
-		ManaHandler.instanceFor(player).updateClient(player, true)
-		LevelHandler.instanceFor(player).updateClient(player, true)
-	}
-	
-	//===== PLAYER TICK =====//
-	
-	@SubscribeEvent def onPlayerTick(e:PlayerTickEvent) {
-		if(e.phase != Phase.END) return
-		
-		if(!e.player.worldObj.isRemote) {
-			updatePlayerMana(e.player)
-			updatePlayerLevel(e.player)
-		}
-	}
-	
-	def updatePlayerMana(player:EntityPlayer) {
-		val mh = ManaHandler.instanceFor(player)
-		
-		if(!player.isDead) mh.regenMana(mh.manaRegen)
-		mh.updateClient(player)
-	}
-	
-	def updatePlayerLevel(player:EntityPlayer) {
-		val lh = LevelHandler.instanceFor(player)
-
-		if(lh.shouldFireLevelUpEvent) MinecraftForge.EVENT_BUS.post(new ChuuniLevelUpEvent(player, lh))
-		lh.updateClient(player)
+		ManaHandler.instanceFor(player).updateClient
+		LevelHandler.instanceFor(player).updateClient
 	}
 	
 	//===== HURT/KILL ENEMY =====//
@@ -78,7 +52,7 @@ class ChuuniEventHandler {
 			val player = e.getSource.getEntity.asInstanceOf[EntityPlayer]
 			val lh = LevelHandler.instanceFor(player)
 			
-			val expRange:(Float,Float) = e.getEntityLiving match {
+			val expRange:(Int,Int) = e.getEntityLiving match {
 				case _:EntityDragon => (95000,150000)
 				case _:EntityWither => (4000,6000)
 				case _:EntityGhast if e.getSource.getDamageType == "fireball" => (750,1250)
@@ -86,15 +60,21 @@ class ChuuniEventHandler {
 				case _:EntityMob => (50,150)
 				case _ => (25,75)
 			}
-			lh.addExp(ThreadLocalRandom.current.nextDouble(expRange._1, expRange._2).toFloat)
+			lh.addExp(ThreadLocalRandom.current.nextInt(expRange._1, expRange._2))
 		}
 	}
 	
 	//===== CHUUNI EVENTS =====//
 	
-	@SubscribeEvent def onPlayerLevelUp(e:ChuuniLevelUpEvent) {
+	@SubscribeEvent def onPlayerLevelUp(e:LevelHandler.LevelUpEvent) {
 		val mh = ManaHandler.instanceFor(e.player)
-		mh.setMaxMana(250*e.lh.level)
+		mh.setMaxMana(250*e.newLevel) //TODO: balance and change regen (change from max +250 per level to max *2 on even, regen *2 on odd?)
+		
+		if(e.player.worldObj.isRemote && e.newLevel > e.oldLevel) e.player.addChatComponentMessage(new TextComponentString("You leveled up!"))
+	}
+	
+	@SubscribeEvent def onPlayerLevelMax(e:LevelHandler.LevelMaxEvent) {
+		if(e.player.worldObj.isRemote) e.player.addChatComponentMessage(new TextComponentString("You hit the maximum level!"))
 	}
 	
 }
