2cf7febdb1b48782d81461f328b44d2ae3036be1
tripl3dogdare <connor@connorjulian.org>
2016-10-24 17:00:22 -0600

    Remove ChuuniLevelUpEvent
    As it is now deprecated along with the old capabilities, it has been
    removed. The equivalent event is now LevelHandler.LevelUpEvent.

--- src/main/scala/chuunimod/event/ChuuniEvent.scala
+++
@@ -1,7 +0,0 @@
-package chuunimod.event
-
-import net.minecraftforge.fml.common.eventhandler.Event
-import net.minecraft.entity.player.EntityPlayer
-import chuunimod.capabilities.LevelHandler
-
-class ChuuniLevelUpEvent(val player:EntityPlayer, val lh:LevelHandler) extends Event
